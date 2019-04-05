76743937089ac81d37bb749e2f8e91c5f711bfdd
Ricky Tam <ricky.tam@workday.com>
2019-04-04 23:36:57 -0700

    cleanup

--- webapp/index.py
+++ webapp/index.py
@@ -10,7 +10,7 @@ import company
 
 navbar = html.Div([dash_dangerously_set_inner_html.DangerouslySetInnerHTML('''
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <img class="pandemic-icon" src='hermione2.png') }}" height="38" width="34"></img>
+    <img src='hermione2.png') }}" height="38" width="34"></img>
     <a class="navbar-brand" href="#">&nbsp;&nbsp;Project Hermione</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -28,9 +28,6 @@ navbar = html.Div([dash_dangerously_set_inner_html.DangerouslySetInnerHTML('''
           <a class="nav-link" href="#">About</a>
         </li>
       </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <a class="btn btn-secondary my-2 my-sm-0" style="display:inline-block" data-toggle="modal" data-target="#myModal">Report Disease</a>
-      </form>
     </div>
   </nav>''')])
 
@@ -98,8 +95,8 @@ body = html.Div(
     [
         dropdownTitles,
         dropdownMenus,
-        html.Div(id="people_row", children=people.layout),
-        html.Div(id="company_row", children=html.Div()),
+        html.Div(id="people_row", children=people.layout, style={"marginBottom": "10"}),
+        html.Div(id="company_row", children=company.layout),
     ],
     style={
         "margin": "2%"
@@ -110,12 +107,12 @@ body = html.Div(
 app.layout = html.Div(
     [
         # html.Link(href="https://use.fontawesome.com/releases/v5.2.0/css/all.css",rel="stylesheet"),
+        # this is the charting css one from the dash example
         html.Link(href="https://cdn.rawgit.com/plotly/dash-app-stylesheets/2d266c578d2a6e8850ebce48fdb52759b2aef506/stylesheet-oil-and-gas.css",rel="stylesheet"),
-        html.Link(href="https://fonts.googleapis.com/css?family=Dosis", rel="stylesheet"),
-        html.Link(href="https://fonts.googleapis.com/css?family=Open+Sans", rel="stylesheet"),
-        html.Link(href="https://fonts.googleapis.com/css?family=Ubuntu", rel="stylesheet"),
         html.Link(href="https://cdn.rawgit.com/amadoukane96/8a8cfdac5d2cecad866952c52a70a50e/raw/cd5a9bf0b30856f4fc7e3812162c74bfc0ebe011/dash_crm.css", rel="stylesheet"),
-        html.Link(href="https://codepen.io/chriddyp/pen/bWLwgP.css", rel="stylesheet"),
+        # html.Link(href="https://fonts.googleapis.com/css?family=Dosis", rel="stylesheet"),
+        # html.Link(href="https://fonts.googleapis.com/css?family=Open+Sans", rel="stylesheet"),
+        # html.Link(href="https://fonts.googleapis.com/css?family=Ubuntu", rel="stylesheet"),
         # html.Link(href="https://codepen.io/chriddyp/pen/bWLwgP.css", rel="stylesheet"),
         html.Link(href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"),
         html.Link(href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/flatly/bootstrap.min.css", rel="stylesheet"),
@@ -123,13 +120,6 @@ app.layout = html.Div(
         body
     ]
 )
-
-@app.callback(
-    Output("company_row", "children"),
-    [Input("company_selector", "value")]
-)
-def render_company(a):
-    return company.layout
 
 if __name__ == "__main__":
     app.run_server(debug=True)
