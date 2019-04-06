74581a210aa9105218b9d0f6143fbe165fd3fd70
Ricky Tam <ricky.tam@workday.com>
2019-04-05 17:01:19 -0700

    updated tab title because it was bothering me

--- webapp/app.py
+++ webapp/app.py
@@ -10,6 +10,7 @@ import dash_html_components as html
 server = Flask(__name__)
 app = dash.Dash(__name__, server=server, external_stylesheets=[dbc.themes.PULSE])
 app.config.suppress_callback_exceptions = True
+app.title = "Project Hermione"
 server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 server.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///:memory:"
 
